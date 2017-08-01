080b0480089b22542745b14f2b77ad12f27922b4
swallsy <smwall84@gmail.com>
2017-08-01 16:31:05 -0400

    helps with remote db

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Successfully started express application!')
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
